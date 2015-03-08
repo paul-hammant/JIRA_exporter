80897c3ea433cb7077a1aeac6db9c491887609e1
Paul Hammant <paul@hammant.org>
2015-03-08 09:00:49 -0400

    open firefox to page in param

--- index.rb
+++ index.rb
@@ -1,9 +1,10 @@
 require "sikulix"
 include Sikulix
 Settings.setShowActions(true)
+App.focus("firefox");
 ImagePath.add("images.sikuli")
 s = Screen.new
-s.click("1424575128978.png")
-sleep 3
-s.write("Hello World")
+s.click("firefoxUrlTextField.png")
+s.paste(ARGV[1]);
+s.type(Key.ENTER);
 # popup 'hi', 'title'
